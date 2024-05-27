635b7078922ec2f072a615ac78cb97eeb8d18880
Enzo <enzo.camilolado@gmail.com>
2024-05-27 19:59:23 +0200

    add files for production and dev environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    API_BASE_URL: '', //no api url in this project :(
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    API_BASE_URL: '', //no api url in this project :(
+}
