934eba19a3fda7fe9503e0db5a989bd3a0553854
Enzo <enzo.camilolado@gmail.com>
2024-05-27 19:02:09 +0200

    add imports on service components

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
